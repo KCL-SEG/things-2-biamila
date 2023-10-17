6b4e58047764a640d655fac88e9d059813a0e0e0
biamila <biancamarcellomilanez@hotmail.com>
2023-10-17 23:51:46 +0100

    Solved Exercise
    - Define a form called ThingForm that contains the fields name, description and quantity
    - Create a test file that will only accept valid inputs
    - Update home view

--- things/tests.py
+++ things/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from .forms import ThingsForm
 
 # Create your tests here.
+def setUp(self):
+    self.form_input = {
+        "name" : "Thing1",
+        "description" : "This is my thing description",
+        "quantity" : 5
+    }
+
+#test 
+def test_valid_input(self):
+    form = ThingsForm(data=self.form_input)
+    self.assertTrue(form.is_valid())
+  
+def test_quantity_must_be_under_50(self):
+    self.form_input["quantity"] = 51
+    form = ThingsForm(data=self.form_input)
+    self.assertFalse(form.is_valid())
+
